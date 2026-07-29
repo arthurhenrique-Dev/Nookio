6309b86b39f1ad11a1913f653b77c52df0ea7ef8
arthurhenrique-Dev <henriquearthur.dev@gmail.com>
2026-07-29 18:32:02 -0300

    feat: initial commit

---
+++ src/main/java/com/henrique/nookio_api/NookioApiApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.nookio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NookioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NookioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/nookio_api/NookioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.nookio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NookioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
