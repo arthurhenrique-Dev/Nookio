a0ca2b7a7deda9267d1aea436730b731bced0cd2
arthurhenrique-Dev <henriquearthur.dev@gmail.com>
2026-07-29 19:54:54 -0300

    feat(properties): add initial property module structure

---
+++ src/main/java/com/henrique/nookio_api/modules/properties/models/PropertyType.java
@@ -0,0 +1,7 @@
+package com.henrique.nookio_api.modules.properties.models;
+
+public enum PropertyType {
+    APARTMENT,
+    HOUSE,
+    STUDIO
+}
---
+++ src/main/java/com/henrique/nookio_api/modules/properties/models/Season.java
@@ -0,0 +1,8 @@
+package com.henrique.nookio_api.modules.properties.models;
+
+public enum Season {
+    SUMMER,
+    FALL,
+    WINTER,
+    SPRING
+}
